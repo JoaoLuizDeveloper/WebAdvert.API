04ba4f7a06e25c3d80aaa6e6a93ec36f3797d04f
JoaoLuizDeveloper <juninhoworld2012@gmail.com>
2020-09-30 22:17:12 -0300

    activating the reference

---
+++ AdvertApi.Models/Services/IAdvertStorageService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvertApi.Models.Services
+{
+    public interface IAdvertStorageService
+    {
+        Task<string> Add(AdvertModel model);
+        Task<bool> Confirm(ConfirmAdvertModel model);
+    }
+}
